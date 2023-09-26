ffe8cee99c682363729d1b5cc00979dacd3bb327
Leandro22sep <leandroromerofuentes@gmail.com>
2023-09-26 17:11:53 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let nombre = prompt("Ingresa un nombre");
+// nombre = nombre + " " + "Romero Fuentes";
+// console.log(nombre);
+
+let n = prompt("Ingrese un número n");
+if(n <= 20){
+    console.log("n es menor o igual a 20")
+} else if(n <= 50){
+    console.log("n es menor o igual a 50")
+}else if(n <= 100){
+    console.log("n es menor o igual a 100")
+}else{
+    console.log("n es mayor a 100")
+}
